5222f05ae201ec01f3210c0701a5cbdd36682919
Daianatk <daiana.campos04@gmail.com>
2019-07-17 23:02:48 -0500

    Cambios en botones

--- src/components/Waiter.js
+++ src/components/Waiter.js
@@ -20,19 +20,10 @@ useEffect(()=>{
     return views
 }, [] )
 
-function onChange(e) {
-    let string = e.target.value
-    let regex = new RegExp(string, 'desayuno')
-    //si es true se guarda en el filtro sino lo elimina
-    //se utiliza virtualDom full javascript
-    let filtered = list.filter(m => regex.test(m.tags))
-    setResults(filtered)
-}
-
     return (
         <div>
         <section className="viewsTotal">
-        <button className="button" onChange={onChange} id="desayuno">Desayuno</button>
+        <button className="button" id="desayuno">Desayuno</button>
         <button className="button" id="options">Almuerzo</button>
         <button className="button" id="options">Hamburguesas</button>
         <button className="button" id="options">Acompañamientos</button>
@@ -46,9 +37,8 @@ function onChange(e) {
                 <div className="card">
             <img className="card_img" src={food.img} alt="Foods"/> 
             <div className="card_info">
-                <div className="card_title"><b>{food.item} </b></div> 
-                <div className="card_category">$ {food.price} </div> 
-                </div>
+                <button className="button"><b>{food.item}</b> ${food.price}</button> 
+            </div>
             </div>
             </div>
 
